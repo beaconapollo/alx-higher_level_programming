0c3140f40dad3c9903a89285ae00960908b11c7b
beaconapollo <down4trust@gmail.com>
2022-06-29 16:54:02 +0300

    convert to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    j = ord('A')
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z')
+            i = ord(c) - ord('a')
+            print("{:s}".format(chr(i + j)), end='')
