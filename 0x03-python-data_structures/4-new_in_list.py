6b1f1aecef38c0fad30721567e987bfd98cce076
mercy83 <down4trust@gmail.com>
2022-07-05 11:16:37 -0700

    replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def new_in_list(list, index, ne):
-    new = list
-    n = len(new)
+    new = []
+    n = len(list)
+    i = 0
     if index >= 0 and index < n:
-        new[index] = ne
+        while i < n:
+            if i == index:
+                new.append(ne)
+            else:
+                new.append(list[i])
+            i += 1
     return(new)
