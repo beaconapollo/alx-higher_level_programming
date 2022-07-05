cbf2f0e0e9809989e2f4edf1f91e819299374cdd
beaconapollo <down4trust@gmail.com>
2022-07-05 15:54:56 +0300

    argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv) - 1
-if (n == 0):
-    print("{:d} arguments.".format(n))
-elif (n == 1):
-    print("{:d} argument:".format(n))
-    print("{:d}: {:s}".format(n, sys.argv[n]))
-else:
-    print("{:d} arguments:".format(n))
-    i = 1
-    while i <= n:
-        print("{:d}: {:s}".format(i, sys.argv[i]))
-        i += 1
+if __name__ == "__main__":
+    from sys import argv
+    n = len(argv) - 1
+    if (n == 0):
+        print("{:d} arguments.".format(n))
+    elif (n == 1):
+        print("{:d} argument:".format(n))
+        print("{:d}: {:s}".format(n, argv[n]))
+    else:
+        print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
