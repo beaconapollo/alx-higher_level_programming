d080d6952c40519a7b63cb3b8317f0a0e80a3bd1
beaconapollo <down4trust@gmail.com>
2022-07-07 11:50:37 +0300

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(dict):
     score = 0
     key = ''
-    if dict == None:
+    if dict == None or dict == {}:
         return(None)
     for i in dict.keys():
         if score <= dict[i]:
