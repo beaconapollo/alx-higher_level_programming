6153415c6c09f96ac5a879892881a2265d77f95e
beaconapollo <down4trust@gmail.com>
2022-07-08 21:45:26 +0300

    multiply by no

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
-
-my_list = [1, 2, 3, 4, 6]
-new_list = multiply_list_map(my_list, 4)
-print(new_list)
-print(my_list)
+def multiply_list_map(my_list=[], number):
+    new_list = []
+    for e in my_list:
+        new_list.append(e * number)
+    return new_list
