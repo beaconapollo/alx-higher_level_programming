e3617cfe5df1d0e3658f9b9d518e3dda5a8afe52
beaconapollo <down4trust@gmail.com>
2022-07-12 10:46:25 +0300

    Print float

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-if (isinstance(number, float)):
-    print(f"Float: {number:.2f}")
-else:
-    print("ValueError: Unknown format code 'd' for object of type 'str'")
+print(f"Float: {number:.2f}")
