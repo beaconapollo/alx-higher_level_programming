77d35be7dadf33e5791a1197b943086e8afd9092
mercy83 <down4trust@gmail.com>
2022-08-07 11:12:28 -0700

    delete list item at index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    l = 0
+    len = 0
     i = 0
 
     for item in my_list:
-        l += 1
+        len += 1
     if idx < 0 or idx >= l:
         return my_list
-    while i < l:
+    while i < len:
         if i == idx:
             del my_list[i]
         i += 1
